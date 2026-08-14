8a8c3311e5d53244ba39f0c7d6a148c204833fb1
Ayush Agrawal <ayushagrawalfzd4321@gmail.com>
2026-08-14 19:48:32 +0530

    Initial Commit

---
+++ src/compoonents/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="bg-cyan-700 w-full flex justify-center items-center h-10">
+        <span>&copy; 2026 Ayush Agrawal. All Rights Reserved.</span>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/compoonents/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+  <nav className='flex justify-between px-2 bg-cyan-500'>
+    <span className='font-extrabold'>iTask</span>
+    <ul className='flex gap-5'>
+      <li className='cursor-pointer duration-200 transition-all hover:font-bold'>Home</li>
+      <li className='cursor-pointer duration-200 transition-all w-20 hover:font-bold'> Your Tasks</li>
+    </ul>
+    
+  </nav>
+  )
+}
+
+export default Navbar
